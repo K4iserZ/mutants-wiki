3e5bc69bfdea21a2447f733c77fb51f4312de01e
K4iserZ <chriszebas@gmail.com>
2026-09-11 16:05:55 -0500

    stats

--- assets/js/core/routing.js
+++ assets/js/core/routing.js
@@ -1,3 +1,18 @@
+let statsEnginePromise;
+
+function loadStatsEngine() {
+  if (window.MGG_STATS) return Promise.resolve();
+  if (statsEnginePromise) return statsEnginePromise;
+  statsEnginePromise = new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = `${window.MGG_SITE_BASE}/assets/js/calculator/stats-engine.js`;
+    script.onload = resolve;
+    script.onerror = () => reject(new Error('Unable to load the stats engine'));
+    document.head.appendChild(script);
+  });
+  return statsEnginePromise;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   window.MGG_TEMPLATES.renderShell();
   document.addEventListener('click', event => {
@@ -8,7 +23,11 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   const path = window.location.pathname.slice(window.MGG_SITE_BASE.length).replace(/\/$/, '');
-  if (/\/specimen\/[^/]+$/i.test(path)) initMutantDetailPage(path.split('/').pop());
+  if (/\/specimen\/[^/]+$/i.test(path)) {
+    loadStatsEngine().then(() => initMutantDetailPage(path.split('/').pop())).catch(error => {
+      document.getElementById('pageContent').innerHTML = errorMessage(error);
+    });
+  }
   else if (path === '/mutants' || path === '/mutants.html') initMutantsPage();
   else window.MGG_HOME.init();
   initQuickSearch();
